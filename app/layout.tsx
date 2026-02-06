1d2ca358e3e8d86a31f4d8e097134b484668bf05
Nicha Hasuwannakit <nichayuyu@gmail.com>
2026-02-05 22:13:13 -0500

    Made sidebar cover 100% height (5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased flex jersey-10-regular">
         <Sidebar />
-        <div className="flex-grow ml-[250px]">
+        <div className="flex-grow">
           {children}
         </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center ml-[250px] mr-[8px] mb-[8px]">
+    <main className="flex min-h-screen flex-col items-center justify-center mr-[8px] mb-[8px]">
       <h1 className="text-5xl font-bold text-gray-800 mb-4">Hello World</h1>
     </main>
   );
